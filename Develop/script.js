6922c3ce9feda1b04ded1f72078bafbc86b2040c
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-15 11:47:29 -0500

    challenge complete

--- Develop/script.js
+++ Develop/script.js
@@ -14,43 +14,67 @@ var passwordInfo =
       var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
       this.charactersUsed = this.charactersUsed.concat(lowercaseArray);
     }
-    console.log(this.charactersUsed);
+    
     if(this.uppercaseInclude == true)
     {
       var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
       this.charactersUsed = this.charactersUsed.concat(uppercaseArray);
 
     }
-    console.log(this.charactersUsed);
+    
     if(this.numericInclude == true)
     {
       var numericArray = ["0","1","2","3","4","5","6","7","8","9"];
       this.charactersUsed = this.charactersUsed.concat(numericArray);
     }
-    console.log(this.charactersUsed);
+
     if(this.specialInclude == true)
     {
       var specialArray = [" ","!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-
+      this.charactersUsed = this.charactersUsed.concat(specialArray);
     }
-    console.log(this.charactersUsed);
+  },
+  passwordInfoReset: function()
+  {
+    this.charactersUsed = [];
+    this.lowercaseInclude = false;
+    this.uppercaseInclude = false;
+    this.numericInclude = false;
+    this.specialInclude = false;
+    this.length = 0;
+  }
+};
+
+var getPasswordLength = function()
+{
+  // Request length of password
+  var passwordLengthString = window.prompt("For characters between 8 and 128, how long would you like your password to be?");
+  passwordInfo.length = parseInt(passwordLengthString);
+  if((passwordInfo.length < 8 || passwordInfo.length > 128) || passwordLengthString === "")
+  {
+    window.alert("Invalid Entry. Please try again.");
+    getPasswordLength();
   }
 };
 
 var generatePassword = function()
 {
-  // Request length of password
-  var passwordLengthString = window.prompt("For characters between 8 and 128, how long would you like your password to be?");
-  passwordInfo.length = parseInt(passwordLengthString);
-  if(passwordInfo.length < 8 || passwordInfo.length > 128)
-  {
-    window.alert("Invalid Entry. Please try again.");
-    generatePassword();
-  }
+  var password = "";
+
+  // Get Password Length
+  getPasswordLength();
 
   // Request Password character set
+  criteriaSelect();
 
-}
+  // Build Password
+  for(var i = 0; i < passwordInfo.length; i++)
+  {
+    password += passwordInfo.charactersUsed[Math.floor(Math.random() * passwordInfo.charactersUsed.length)];
+  }
+
+  return password;
+};
 
 // Determine Password Criteria
 var criteriaSelect = function()
@@ -75,11 +99,14 @@ var criteriaSelect = function()
   if((passwordInfo.lowercaseInclude || passwordInfo.uppercaseInclude || passwordInfo.numericInclude || passwordInfo.specialInclude) == false)
   {
     window.alert("You must select at least one characterset for your password. Try Again.");
-    criteriaSelected();
+    criteriaSelect();
   }
 
+  // Builds an array of usable charaters 
+  passwordInfo.createCharacterArray();
+
   return;
-}
+};
 
 
 // Get references to the #generate element
@@ -87,6 +114,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  passwordInfo.passwordInfoReset();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
